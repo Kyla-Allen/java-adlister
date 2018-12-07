264cc94b93c597014f7b542d1acd6b7449c856df
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-07 15:22:01 -0600

    added page view counter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,17 +6,41 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/count")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
-        res.setContentType("text/html");
+    private int hitCount;
+
+    public void init(){
+        int hitCount = 0;
+    }
+
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+//
+        res.setContentType("text/htmml");
+        hitCount ++;
         PrintWriter out = null;
         try {
             out = res.getWriter();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        out.println("<h1> Hello, World! </h1>");
+        String html = "Current Count: " + hitCount;
+        out.printf( html );
+
+
+//        res.setContentType("text/html");
+//        PrintWriter out = null;
+//        try {
+//            out = res.getWriter();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        out.println("<h1> Hello, World! </h1>");
+
+
+
     }
 }
