37641579046467c2613e4c48bdcd705a2f9d28c8
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-12 12:27:18 -0600

    locked down the profile page

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,20 +3,47 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
+//    DO GET==========================================
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+//        Session Object
+        HttpSession ses = request.getSession();
+
+//        Default Value for user
+        if(ses.getAttribute("user") == null){
+            ses.setAttribute("user", false);
+        }
+
+//        Convert to Boolean
+        Boolean user = (Boolean) ses.getAttribute("user");
+
+
+//        Checking to see if I let the user see the login page or toss em
+        if(user){
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
 
+
+
+//    DO POST============================================
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        Session Object
+        HttpSession ses = request.getSession();
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            ses.setAttribute("user", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+//        Session Stuff
+        HttpSession ses = request.getSession();
+        Boolean isUser = (Boolean) ses.getAttribute("user");
+
+        if (isUser == null) {
+            isUser = false;
+        }
+
+
+        if(isUser) {
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
+
     }
 }
